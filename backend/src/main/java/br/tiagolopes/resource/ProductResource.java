5e86ca1c666653090c98615ad23773f61bbaaa25
Tiago Lopes <tiagoltavares2002@gmail.com>
2026-02-17 14:40:49 -0300

    feature: implementando os endpoints para edição e exclusão de um produto

--- backend/src/main/java/br/tiagolopes/resource/ProductResource.java
+++ backend/src/main/java/br/tiagolopes/resource/ProductResource.java
@@ -33,6 +33,52 @@ public class ProductResource {
         product.name = dto.name();
         product.price = dto.price();
 
+        addProductComposition(dto, product);
+
+        product.persist();
+
+        return Response.status(Response.Status.CREATED).entity(product).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    public Response update(@PathParam("id") Long id, ProductDTO dto) {
+        Product product = Product.findById(id);
+
+        if (product == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        product.name = dto.name();
+        product.price = dto.price();
+        product.composition.clear();
+
+        addProductComposition(dto, product);
+
+        return Response.ok(product).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response delete(@PathParam("id") Long id) {
+        boolean deleted = Product.deleteById(id);
+
+        if (deleted) {
+            return Response.noContent().build();
+        }
+
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @GET
+    @Path("/production-plan")
+    public ProductionPlanDTO getProductionPlan() {
+        return productionService.calculateProductionPlan();
+    }
+
+    private void addProductComposition(ProductDTO dto, Product product) {
         if (dto.composition() != null) {
             dto.composition().forEach(item -> {
                 RawMaterial material = RawMaterial.findById(item.rawMaterialId());
@@ -47,15 +93,5 @@ public class ProductResource {
                 }
             });
         }
-
-        product.persist();
-
-        return Response.status(Response.Status.CREATED).entity(product).build();
-    }
-
-    @GET
-    @Path("/production-plan")
-    public ProductionPlanDTO getProductionPlan() {
-        return productionService.calculateProductionPlan();
     }
 }
